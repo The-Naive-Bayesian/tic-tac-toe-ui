0e51660490ee1bdd339458928100366fd2fcad2f
Chris Emerson <chris.emerson@roledex.com>
2018-10-31 20:25:38 -0600

    Updates

---
+++ precache-manifest.19baaee27a2f61c78fe3e5c46a1ce1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "595a39de80185760f11e",
+    "url": "/tic-tac-toe-ui/static/js/runtime~main.595a39de.js"
+  },
+  {
+    "revision": "80561887714de322da1b",
+    "url": "/tic-tac-toe-ui/static/js/main.80561887.chunk.js"
+  },
+  {
+    "revision": "18e5da24c86da47a39b5",
+    "url": "/tic-tac-toe-ui/static/js/1.18e5da24.chunk.js"
+  },
+  {
+    "revision": "80561887714de322da1b",
+    "url": "/tic-tac-toe-ui/static/css/main.9f7ef350.chunk.css"
+  },
+  {
+    "revision": "34e0b5fcfb4680841497b29629b9f743",
+    "url": "/tic-tac-toe-ui/index.html"
+  }
+];
